5bf7920cfe4632f321767bc292954111a9d41064
hwaterke <hwaterke@users.noreply.github.com>
2017-02-19 14:37:57 +0100

    Constants for the colours of the app

---
+++ client/src/constants/colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+  headerColor: '#89cff0',
+  headerTintColor: 'white',
+  tintColor: '#2f95dc',
+  backgroundColor: 'white',
+  darkPrimaryColor: '#75b9d6',
+  veryDarkPrimaryColor: '#65a9c6',
+  discreteTextColor: '#999'
+};
