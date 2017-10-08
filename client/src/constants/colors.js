5072e78c8d5ed041263f614b060c26d2e8598c2b
hwaterke <hwaterke@users.noreply.github.com>
2017-10-08 18:24:15 +0200

    Cleanup

--- client/src/constants/colors.js
+++ client/src/constants/colors.js
@@ -7,19 +7,3 @@ export const colors = {
   darkPrimaryColor: '#80CBC4',
   discreteTextColor: '#999'
 };
-
-export const gradientForType = type => {
-  switch (type) {
-    case 'normal':
-      return ['#eacda3', '#d6ae7b'];
-    case 'electric':
-      return ['#f1c40f', '#ffb347'];
-    case 'water':
-      return ['#A7BFE8', '#6190E8'];
-    case 'fire':
-      return ['#DE6262', '#FFB88C'];
-    case 'poison':
-      return ['#B993D6', '#8CA6DB'];
-  }
-  return ['#DAD299', '#B0DAB9'];
-};
