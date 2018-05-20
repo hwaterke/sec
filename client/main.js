4d5489f2953b8a8ff760f7b04f25ab2d6def05da
Antonio Calapez <antonio.root@gmail.com>
2018-05-20 21:40:14 +0200

    feat(Firebase): Setup firebase

--- client/main.js
+++ client/main.js
@@ -1,5 +1,9 @@
 /* eslint-disable import/default */
 import Expo from 'expo';
+import firebase from 'firebase';
 import {App} from './src/components/App';
+import {firebaseConfig} from './src/constants/firebase';
+
+firebase.initializeApp(firebaseConfig);
 
 Expo.registerRootComponent(App);
