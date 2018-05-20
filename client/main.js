425f9754a1c3d5328bc7c3daee3559efcaa06bbb
Antonio Calapez <antonio.root@gmail.com>
2018-05-20 22:45:03 +0200

    feat(Firestore): Setup the firestore

--- client/main.js
+++ client/main.js
@@ -1,9 +1,13 @@
 /* eslint-disable import/default */
 import Expo from 'expo';
 import firebase from 'firebase';
+// required for side-effects
+require('firebase/firestore');
 import {App} from './src/components/App';
 import {firebaseConfig} from './src/constants/firebase';
 
 firebase.initializeApp(firebaseConfig);
+
+firebase.firestore().settings({timestampsInSnapshots: true});
 
 Expo.registerRootComponent(App);
