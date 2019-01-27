73a26dc65c1b27626ebb63d9eff71b81a9de100c
hwaterke <hwaterke@users.noreply.github.com>
2019-01-27 12:18:23 +0100

    Improved time since last set

--- client/src/components/screens/today/TimeSince.js
+++ client/src/components/screens/today/TimeSince.js
@@ -3,6 +3,22 @@ import PropTypes from 'prop-types'
 import moment from 'moment'
 import styled from 'styled-components'
 
+const intDivision = (a, b) => Math.floor(a / b)
+
+const secondsToHuman = seconds => {
+  const deltas = [[24 * 60 * 60, 'd'], [60 * 60, 'h'], [60, 'm']]
+
+  for (const [delta, suffix] of deltas) {
+    if (seconds >= delta) {
+      return `${intDivision(seconds, delta)}${suffix} ${secondsToHuman(
+        seconds % delta
+      )}`
+    }
+  }
+
+  return `${seconds}s`
+}
+
 const Text = styled.Text`
   font-size: 40px;
 `
@@ -33,12 +49,6 @@ export class TimeSince extends React.Component {
       moment.duration(this.state.now.diff(this.props.time)).as('seconds')
     )
 
-    return (
-      <Text>
-        {seconds > 60
-          ? `${Math.floor(seconds / 60)}m ${seconds % 60}s`
-          : `${seconds}s`}
-      </Text>
-    )
+    return <Text>{secondsToHuman(seconds)}</Text>
   }
 }
