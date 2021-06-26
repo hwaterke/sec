ad7bcfc713349beaa5c0a4efe1d4f58bb664a045
hwaterke <hwaterke@users.noreply.github.com>
2021-06-26 16:28:56 +0200

    Fixes AsyncStorage import

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,5 +1,5 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit'
-import {AsyncStorage} from 'react-native'
 import {persistReducer, persistStore} from 'redux-persist'
 import {rootReducer} from './reducers/rootReducer'
 
