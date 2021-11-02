b7d6ee1da110301eca5436c124c12cafaa0ecb3d
hwaterke <hwaterke@users.noreply.github.com>
2021-11-02 16:59:56 +0100

    Adds backend validation on inputs

--- backend/src/workout_sets/workout_set.input.ts
+++ backend/src/workout_sets/workout_set.input.ts
@@ -1,4 +1,5 @@
 import {Field, InputType} from '@nestjs/graphql'
+import {IsInt, IsPositive, Matches} from 'class-validator'
 
 @InputType()
 export class WorkoutSetInput {
@@ -8,15 +9,20 @@ export class WorkoutSetInput {
   @Field()
   executedAt!: Date
 
+  @IsInt()
+  @IsPositive()
   @Field({nullable: true})
   repetitions?: number
 
   @Field({nullable: true})
   weight?: number
 
+  @Matches(/^\d\d:\d\d:\d\d$/)
   @Field({nullable: true})
   time?: string
 
+  @IsInt()
+  @IsPositive()
   @Field({nullable: true})
   distance?: number
 
@@ -29,15 +35,20 @@ export class WorkoutSetUpdateInput {
   @Field()
   executedAt!: Date
 
+  @IsInt()
+  @IsPositive()
   @Field({nullable: true})
   repetitions?: number
 
   @Field({nullable: true})
   weight?: number
 
+  @Matches(/^\d\d:\d\d:\d\d$/)
   @Field({nullable: true})
   time?: string
 
+  @IsInt()
+  @IsPositive()
   @Field({nullable: true})
   distance?: number
 
