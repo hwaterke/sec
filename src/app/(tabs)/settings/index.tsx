404297632a1b4cf5231b18bef2043cead33082be
hwaterke <hwaterke@users.noreply.github.com>
2025-03-17 20:35:09 +0100

    Add confirmation modal for clearing the database

--- src/app/(tabs)/settings/index.tsx
+++ src/app/(tabs)/settings/index.tsx
@@ -15,9 +15,25 @@ export default function SettingsHome() {
         Import database
       </Button>
       <Button
-        onPress={async () => {
-          await DatabaseService.resetDatabase()
-          Alert.alert('Database reset')
+        onPress={() => {
+          Alert.alert(
+            'Reset Database',
+            'Are you sure you want to reset the database? This action cannot be undone.',
+            [
+              {
+                text: 'Cancel',
+                style: 'cancel',
+              },
+              {
+                text: 'Reset',
+                style: 'destructive',
+                onPress: async () => {
+                  await DatabaseService.resetDatabase()
+                  Alert.alert('Success', 'Database has been reset')
+                },
+              },
+            ]
+          )
         }}
       >
         Reset database
