92f6ee6fa83a21ff3df192e05cf6a200d65ec2ab
hwaterke <hwaterke@users.noreply.github.com>
2026-05-07 10:33:22 +0200

    Expo 55

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,21 +1,21 @@
-import {Icon, Label, NativeTabs} from 'expo-router/unstable-native-tabs'
+import {NativeTabs} from 'expo-router/unstable-native-tabs'
 
 export default function TabsLayout() {
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="(exercises)">
-        <Label>Exercises</Label>
-        <Icon sf="list.dash" />
+        <NativeTabs.Trigger.Label>Exercises</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="list.dash" />
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="history">
-        <Label>History</Label>
-        <Icon sf="calendar" />
+        <NativeTabs.Trigger.Label>History</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="calendar" />
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="settings">
-        <Label>Settings</Label>
-        <Icon sf="gear" />
+        <NativeTabs.Trigger.Label>Settings</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="gear" />
       </NativeTabs.Trigger>
     </NativeTabs>
   )
